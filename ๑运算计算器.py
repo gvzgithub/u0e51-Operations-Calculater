2be3a6c0fe5d48a4b4ba2cd9bd952669f4e126b7
gvzgithub <143880575+gvzgithub@users.noreply.github.com>
2024-04-21 10:38:48 +0800

    Add files via upload

--- ๑运算计算器.py
+++ ๑运算计算器.py
@@ -1,17 +1,34 @@
-while True:
+def operate(a, b, x, y):
+    x_prime = a * x + b * y
+    y_prime = a * x - b * y
+    return x_prime, y_prime
+
+
+def main():
     print("欢迎使用本计算器！")
-    a = float(input("请输入a的值: "))
-    b = float(input("请输入b的值: "))
-    x = float(input("请输入x的值: "))
-    y = float(input("请输入y的值: "))
+    while True:
+        a = input("请输入a的值：")
+        b = input("请输入b的值：")
+        x = input("请输入x的值：")
+        y = input("请输入y的值：")
 
-    try:
-        x_prime = a * x + b * y
-        y_prime = a * x - b * y
-        print(f"经过运算后的结果是: ({x_prime}, {y_prime})")
+        try:
+            a = float(a)
+            b = float(b)
+            x = float(x)
+            y = float(y)
 
-        choice = input("是否要再来一遍运算? 按1继续, 其他键退出: ")
-        if choice != "1":
-            break
-    except:
-        print("无法运算,请输入其他数值!")
+            x_prime, y_prime = operate(a, b, x, y)
+            print(f"运算结果：({x_prime}, {y_prime})")
+
+            choice = input("按数字1键决定是否要再来一遍运算๑（按其他键退出）：")
+            if choice != '1':
+                break
+
+        except ValueError:
+            print("无法运算，请输入其他数值！")
+            continue
+
+
+if __name__ == "__main__":
+    main()
