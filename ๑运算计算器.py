82e28aa900f055f048a7be33c3d28cbd2d4be587
gvzgithub <143880575+gvzgithub@users.noreply.github.com>
2024-04-26 21:54:49 +0800

    Add files via upload

--- ๑运算计算器.py
+++ ๑运算计算器.py
@@ -5,12 +5,35 @@ def operate(a, b, x, y):
 
 
 def main():
-    print("欢迎使用本计算器！")
+    language = input("Please select language: Press 1 for English, Press 2 for Chinese: ")
+
+    if language == "1":
+        print("Welcome to this calculator!")
+        prompt_a = "Please enter the value of a: "
+        prompt_b = "Please enter the value of b: "
+        prompt_x = "Please enter the value of x: "
+        prompt_y = "Please enter the value of y: "
+        error_msg = "Unable to perform calculation, please enter different values!"
+        repeat_msg = "Press 1 to perform another calculation, or any other key to exit: "
+
+    elif language == "2":
+        print("欢迎使用本计算器！")
+        prompt_a = "请输入a的值："
+        prompt_b = "请输入b的值："
+        prompt_x = "请输入x的值："
+        prompt_y = "请输入y的值："
+        error_msg = "无法运算，请输入其他数值！"
+        repeat_msg = "按数字1键决定是否要再来一遍运算๑（按其他键退出）："
+
+    else:
+        print("Invalid language selection. Exiting...")
+        return
+
     while True:
-        a = input("请输入a的值：")
-        b = input("请输入b的值：")
-        x = input("请输入x的值：")
-        y = input("请输入y的值：")
+        a = input(prompt_a)
+        b = input(prompt_b)
+        x = input(prompt_x)
+        y = input(prompt_y)
 
         try:
             a = float(a)
@@ -19,14 +42,14 @@ def main():
             y = float(y)
 
             x_prime, y_prime = operate(a, b, x, y)
-            print(f"运算结果：({x_prime}, {y_prime})")
+            print(f"Result: ({x_prime}, {y_prime})")
 
-            choice = input("按数字1键决定是否要再来一遍运算๑（按其他键退出）：")
+            choice = input(repeat_msg)
             if choice != '1':
                 break
 
         except ValueError:
-            print("无法运算，请输入其他数值！")
+            print(error_msg)
             continue
 
 
