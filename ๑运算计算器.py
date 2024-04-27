16d01c3833050679667a5f2537b0d8dfe898a562
gvzgithub <143880575+gvzgithub@users.noreply.github.com>
2024-04-27 19:50:45 +0800

    Add files via upload

--- ๑运算计算器.py
+++ ๑运算计算器.py
@@ -1,11 +1,21 @@
+import time
+
 def operate(a, b, x, y):
     x_prime = a * x + b * y
     y_prime = a * x - b * y
     return x_prime, y_prime
 
+prompt_a = ""
+prompt_b = ""
+prompt_x = ""
+prompt_y = ""
+error_msg = ""
+repeat_msg = ""
+invalid_choice_msg = ""
 
 def main():
-    language = input("Please select language: Press 1 for English, Press 2 for Chinese: ")
+    global prompt_a, prompt_b, prompt_x, prompt_y, error_msg, repeat_msg, invalid_choice_msg
+    language = input("Please select language: Press 1 for English, Press 2 for Chinese(Entering another number will exit):")
 
     if language == "1":
         print("Welcome to this calculator!")
@@ -15,6 +25,7 @@ def main():
         prompt_y = "Please enter the value of y: "
         error_msg = "Unable to perform calculation, please enter different values!"
         repeat_msg = "Press 1 to perform another calculation, or any other key to exit: "
+        invalid_choice_msg = "Invalid language selection. Exiting in 3 seconds..."
 
     elif language == "2":
         print("欢迎使用本计算器！")
@@ -24,9 +35,11 @@ def main():
         prompt_y = "请输入y的值："
         error_msg = "无法运算，请输入其他数值！"
         repeat_msg = "按数字1键决定是否要再来一遍运算๑（按其他键退出）："
+        invalid_choice_msg = "无效的语言选择。3秒后退出..."
 
     else:
-        print("Invalid language selection. Exiting...")
+        print(invalid_choice_msg)
+        time.sleep(2)
         return
 
     while True:
@@ -51,7 +64,6 @@ def main():
         except ValueError:
             print(error_msg)
             continue
-
 
 if __name__ == "__main__":
     main()
